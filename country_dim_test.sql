7528cf8a5e055511e3c88cbda564d0705ff16a07
Roja383 <139374516+Roja383@users.noreply.github.com>
2023-08-28 20:32:15 -0500

    Add files via upload

---
+++ country_dim_test.sql
@@ -0,0 +1,12 @@
+CREATE TABLE resin_mart.country_dim_test (
+	country_key int8 NOT NULL,
+	country_cd text NOT NULL,
+	country_desc text NOT NULL,
+	iso_3 text NOT NULL,
+	iso_n int4 NULL,
+	data_inserted_by text NULL DEFAULT 'SYSTEM'::text,
+	data_inserted_at timestamp NULL DEFAULT now(),
+	data_updated_by text NULL DEFAULT 'SYSTEM'::text,
+	data_updated_at timestamp NULL DEFAULT now(),
+	CONSTRAINT country_dim_pkey PRIMARY KEY (country_key)
+);
